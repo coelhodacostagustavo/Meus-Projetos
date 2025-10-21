06fdd42d1076f7b70af8381a65b9e3c8b79f215e
Gustavo Costa <coelhodacostagustavo@gmail.com>
2025-10-21 16:52:24 -0300

    Remover pasta Desafio Contador Interativo de Caracteres

--- Desafio Contador Interativo de Caracteres/Páginas de Perguntas Frequentes/js/script.js
+++
@@ -1,9 +0,0 @@
-function alternarResposta(elemento) {
-    //querySelector é o método que econtra o primeiro elemento filho que corresponda ao seletor CSS .resposta
-    const resposta = elemento.querySelector('.resposta');
-    //método toggle  é chamado no classList do elemento resposta, adicionando a classe mostrar se não estiver presente ou remove se estiver aplicada
-    resposta.classList.toggle('mostrar');
-
-    resposta.classList.toggle('escondido');
-
-}
